702f1f18202448154bd95c016b07688af10f9d95
pit1988 <gabelli29@hotmail.com>
2016-02-07 12:41:37 +0100

    fatta classe LibraryItem

--- libraryitem.cpp
+++ libraryitem.cpp
@@ -1,5 +1,30 @@
 #include "libraryitem.h"
 
-LibraryItem::LibraryItem()
-{
+LibraryItem::LibraryItem(): titolo(""), autore(""), annoCreazione(0) {}
+
+LibraryItem::LibraryItem(QString t ="", QString au ="", int a =0): titolo(t), autore(au), anno(a) {}
+
+LibraryItem::~LibraryItem(){} //posso anche evitare di scriverlo, visto che è virtuale puro!
+
+bool LibraryItem::operator ==(const LibraryItem& li) const{
+    return (titolo == li.titolo && autore == li.autore && annoCreazione == li.annoCreazione);
 }
+
+bool LibraryItem::operator !=(const LibraryItem& li) const{
+    return !(titolo == li.titolo && autore == li.autore && annoCreazione == li.annoCreazione);
+}
+
+QString LibraryItem::getTitolo() const{
+    return titolo;
+}
+
+QString LibraryItem::getAutore() const{
+    return autore;
+}
+
+int LibraryItem::getAnno() const{
+    return annoCreazione;
+}
+
+
+
