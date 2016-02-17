d2c23edb184737582110b140a8550cca73f015d9
pit1988 <gabelli29@hotmail.com>
2016-02-17 14:05:25 +0100

    Inizio

--- View/mywidget.h
+++ View/mywidget.h
@@ -1,4 +1,3 @@
-
 #ifndef MYWIDGET_H
 #define MYWIDGET_H
 
@@ -9,22 +8,66 @@
 #include <QPushButton>
 #include <QSize>
 #include <QSpacerItem>
+#include <QStringList>
 #include <QTableWidget>
 #include <QVBoxLayout>
 #include <QWidget>
 
-class MyWidget : public QWidget
-{
-    Q_OBJECT
+#include "../Model/container.h"
+#include "../Model/smartptr.h"
+#include "../Model/libraryitem.h"
+#include "../Model/cd.h"
+#include "../Model/dvd.h"
+#include "../Model/film.h"
+#include "../Model/libro.h"
+#include "../Model/vhs.h"
+
+#include "../View/"
+
+class MyWidget : public QWidget {
+  Q_OBJECT
 public:
-    explicit MyWidget(QWidget *parent = 0);
-    ~MyWidget();
-    void setupView();
-    Container* getContainer() const;
-signals:
+  MyWidget(QWidget* parent = 0);
+  ~MyWidget() {}
+private:
+  Biblio bib;
+  QTableWidget* tableWidget;
+  QStringList tableWidgetHeader;
+  int monitorWidth;
+  int monitorHeight;
+  QPushButton* qpb_manage_st;
+  QPushButton* qpb_manage_di;
+  QPushButton* qpb_find_o;
+  QPushButton* qpb_quit;
+  QSpacerItem* horizontalSpacer;
+  QVBoxLayout* verticalLayout;
+  QGridLayout* gridLayout;
+
+  Gestione_CD* mCd;
+  Gestione_DVD* mDvd;
+  Gestione_VHS* mVHS;
+  Gestione_Libro* mLibro;
+  Trova_LibraryItem* fli;
 
 public slots:
-
+	void slotGestioneCdQDialog();
+	void slotGestioneDvdQDialog();
+	void slotGestioneVhsQDialog();
+	void slotGestioneLibroQDialog();
+  void slotFindItemQDialog();
+  void slotInsertCD();
+  void slotInsertDVD();
+  void slotInsertVHS();
+  void slotInsertLibro();
+  void slotRemoveCD();
+  void slotRemoveDVD();
+  void slotRemoveVHS();
+  void slotRemoveLibro();
+  void slotReplaceCD();
+  void slotReplaceDVD();
+  void slotReplaceVHS();
+  void slotReplaceLibro();
+  void updateTableResult();
 };
 
-#endif // MYWIDGET_H
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "View/mainwindow.h"
+#include "View/mywidget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MyWidget w;
     w.show();
     return a.exec();
 }
